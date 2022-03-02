e3f1a58af84b4b9a4f167d305c0c132be5962958
zflovcoding <2043166212@qq.com>
2022-03-02 18:08:18 +0800

    logout function done

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.nowcoder.community.controller;
 
 import com.google.code.kaptcha.Producer;
-import com.nowcoder.community.config.KaptchaConfig;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
@@ -12,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,52 +38,55 @@ public class LoginController implements CommunityConstant {
     private String contextPath;
 
     //the funtion which is when user clicked "注册" in home page,we should direct to /register page
-    @RequestMapping(path="/register",method = RequestMethod.GET)
-    public String accessRegisterPage(){
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String accessRegisterPage() {
         return "/site/register";
     }
+
     //get the login page method
-    @RequestMapping(path = "/login",method = RequestMethod.GET)
-    public String accessLoginPage(){
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String accessLoginPage() {
         return "/site/login";
     }
-    @RequestMapping(path="/register",method = RequestMethod.POST)
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
     //只要页面上传入的值和User的属性相匹配，SpringMVC就会自动把值注入进user
-    public String register(Model model, User user){
+    public String register(Model model, User user) {
         Map<String, Object> map = userService.register(user);
-        if(map==null || map.isEmpty()){
+        if (map == null || map.isEmpty()) {
             //此时说明注册成功
             //成功后，页面跳转到首页（第三方页面：operate-result.html，显示页面在多少秒后跳转（转到首页））
             //激活后才跳转到登陆页面
-            model.addAttribute("msg","注册成功，我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
-            model.addAttribute("target","/index");//成功后跳转首页
+            model.addAttribute("msg", "注册成功，我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
+            model.addAttribute("target", "/index");//成功后跳转首页
             //此处返回这个中间界面的路径
             return "/site/operate-result";
 
-        }else{
+        } else {
             //注册失败，把失败的信息，也就是Map里的Msg返回
             //key是字符串
             //有的结果可能是空的，直接把可能的信息都返回
-            model.addAttribute("usernameMsg",map.get("usernameMsg"));
-            model.addAttribute("passwordMsg",map.get("passwordMsg"));
-            model.addAttribute("emailMsg",map.get("emailMsg"));
+            model.addAttribute("usernameMsg", map.get("usernameMsg"));
+            model.addAttribute("passwordMsg", map.get("passwordMsg"));
+            model.addAttribute("emailMsg", map.get("emailMsg"));
             //return to the register page
             return "/site/register";
         }
     }
+
     //http://localhost:15213/community/activation/101/code
-    @RequestMapping(path = "/activation/{userID}/{code}",method = RequestMethod.GET)
+    @RequestMapping(path = "/activation/{userID}/{code}", method = RequestMethod.GET)
     //@PathVariable annotation means get variables from path
     //激活成功应该跳转到operate-result页面.不同的处理结果给operate-result不同的target
-    public String activation(Model model ,@PathVariable("userID") int userId,@PathVariable("code") String code){
+    public String activation(Model model, @PathVariable("userID") int userId, @PathVariable("code") String code) {
         int result = userService.activate(userId, code);
-        if(result==ACTIVATION_REPEAT){
+        if (result == ACTIVATION_REPEAT) {
             model.addAttribute("msg", "无效操作,该账号已经激活过了!");
             model.addAttribute("target", "/index");
-        }else if(result==ACTIVATION_SUCCESS){
+        } else if (result == ACTIVATION_SUCCESS) {
             model.addAttribute("msg", "激活成功,您的账号已经可以正常使用了!");
             model.addAttribute("target", "/login");
-        }else {
+        } else {
             model.addAttribute("msg", "激活失败,您提供的激活码不正确!");
             model.addAttribute("target", "/index");
         }
@@ -95,14 +98,14 @@ public class LoginController implements CommunityConstant {
     //return a image to the page
     //输出图片到浏览器需要使用response，验证码是一个敏感文件，存在Cookie会有敏感信息的安全问题
     //所以存储到session里，之后会使用redis重构
-    @RequestMapping(path="/kaptcha",method = RequestMethod.GET)
-    public void getKaptcha(HttpServletResponse response, HttpSession session){
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
         //生成验证码，用Spring管理的容器注入配置
         //首先设置文本，然后把文本传入，创建验证码Image
         String text = kaptcha.createText();
         BufferedImage image = kaptcha.createImage(text);
         //将文本验证码存入session
-        session.setAttribute("kaptcha",text);
+        session.setAttribute("kaptcha", text);
         //将图片直接输出到浏览器
         //首先设置response返回内容的类型
         response.setContentType("image.png");
@@ -110,23 +113,23 @@ public class LoginController implements CommunityConstant {
             ServletOutputStream os = response.getOutputStream();
             //输出图片的工具
             //三个参数：1.要输出的图片 2.格式 3.使用的流
-            ImageIO.write(image,"png",os);
+            ImageIO.write(image, "png", os);
             //response是由SpringMVC管理的，不需要自己手动关闭os流
         } catch (IOException e) {
             //出现异常，输出日志
-            logger.error("响应验证码失败:"+e.getMessage());
+            logger.error("响应验证码失败:" + e.getMessage());
         }
 
     }
 
     /**
-     * @param username  用户名
-     * @param password 密码
-     * @param code 验证码
+     * @param username   用户名
+     * @param password   密码
+     * @param code       验证码
      * @param rememberMe 是否勾选记住我，决定了凭证的过期时间·长短
-     * @param model model封装保存
-     * @param session 之前用session记录保存的验证码，此处做一个校验
-     * @param response 返回的ticket要存到cookie里，需要使用response
+     * @param model      model封装保存
+     * @param session    之前用session记录保存的验证码，此处做一个校验
+     * @param response   返回的ticket要存到cookie里，需要使用response
      * @return 返回路径
      */
     //上面也有一个请求方法Request Mapping是”/login“,是可以重复的，只要请求方法不同
@@ -135,23 +138,23 @@ public class LoginController implements CommunityConstant {
                         Model model, HttpSession session, HttpServletResponse response) {
         //首先判断验证码
         //先拿到之前保存的验证码
-        String kaptcha = (String)session.getAttribute("kaptcha");
+        String kaptcha = (String) session.getAttribute("kaptcha");
         //判断空值并忽略大小写比较，如果有任何不满足则验证码不对，依旧返回登陆页面
-        if(StringUtils.isBlank(kaptcha) || StringUtils.isBlank(code) || !kaptcha.equalsIgnoreCase(code)){
+        if (StringUtils.isBlank(kaptcha) || StringUtils.isBlank(code) || !kaptcha.equalsIgnoreCase(code)) {
             //给页面返回的提示放在model里
-            model.addAttribute("codeMsg","验证码不正确");
+            model.addAttribute("codeMsg", "验证码不正确");
             return "/site/login";
         }
         //验证账号和密码
         //定义两个常量比较方便
-        long expiredSeconds = rememberMe?REMEMBER_ME_EXPIRED_SECONDS:DEFAULT_EXPIRED_SECONDS;
+        long expiredSeconds = rememberMe ? REMEMBER_ME_EXPIRED_SECONDS : DEFAULT_EXPIRED_SECONDS;
         Map<String, Object> loginMap = userService.login(username, password, expiredSeconds);
         //根据是否有ticket判断成功还是失败
-        if(loginMap.containsKey("ticket")){
+        if (loginMap.containsKey("ticket")) {
             //成功:把凭证通过cookie发送到客户端
-            Cookie cookie = new Cookie("ticket",loginMap.get("ticket").toString());
+            Cookie cookie = new Cookie("ticket", loginMap.get("ticket").toString());
             cookie.setPath(contextPath);//cookie都要设置访问路径，此处设置成动态的全项可访问
-            cookie.setMaxAge((int)expiredSeconds);//设置cookie过期时间
+            cookie.setMaxAge((int) expiredSeconds);//设置cookie过期时间
             response.addCookie(cookie);//使用response发生送到客户端
             //返回重定向到首页
             /*关于此处为什么要重定向：
@@ -165,7 +168,7 @@ public class LoginController implements CommunityConstant {
             给浏览器一个响应，并建议浏览器访问另外的组件，以刷新页面的内容。浏览器发出第二次请求，是独立的，和第一次无关的请求。*/
             return "redirect:/index";
 
-        }else{
+        } else {
             //失败
             model.addAttribute("usernameMsg", loginMap.get("usernameMsg"));
             model.addAttribute("passwordMsg", loginMap.get("passwordMsg"));
@@ -175,12 +178,14 @@ public class LoginController implements CommunityConstant {
 
 
     }
-
-
-
-
-
-
+    //该方法需要得到浏览器之前保存的cookie
+    @RequestMapping(path = "/logout",method = RequestMethod.GET)
+    //此处通过@CookieValue注解让SpringMVC把cookie的值注入进来
+    public String logout(@CookieValue("ticket") String ticket){
+        userService.logout(ticket);
+        //请求结束后，重定向到login界面
+        return "redirect:/login";//默认会走GET请求的login
+    }
 
 
 
--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -40,44 +40,44 @@ public class UserService implements CommunityConstant {
     private LoginTicketMapper loginTicketMapper;
 
     //due to DiscussPost , we should get user by userId
-    public User getUserByID(int userID){
+    public User getUserByID(int userID) {
         return userMapper.selectById(userID);
     }
 
     //写一个注册方法:返回的内容是多种信息，账号不能重复，不能为空，等等等的很多信息，所以要返回集合做好封装
     //帮助我们把传来的用户数据存到数据库里面
     //具体包括：用户查重，加密密码，发送用户激活码
-    public Map<String,Object> register(User user){
-        Map<String,Object> map = new HashMap<>();
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
         //空值处理
-        if(user==null){
+        if (user == null) {
             throw new IllegalArgumentException("参数不能为空！");
         }
-        if(StringUtils.isBlank(user.getUsername())){
-            map.put("usernameMsg","账号不能为空");
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "账号不能为空");
             return map;//直接返回消息
         }
-        if(StringUtils.isBlank(user.getPassword())){
-            map.put("passwordMsg","密码不能为空");
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "密码不能为空");
             return map;
         }
-        if(StringUtils.isBlank(user.getEmail())){
-            map.put("emailMsg","邮箱不能为空");
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空");
             return map;
         }
         //验证账号
         User u = userMapper.selectByName(user.getUsername());
-        if(u!=null){
+        if (u != null) {
             //账号存在
-            map.put("usernameMsg","该账号已存在.");
+            map.put("usernameMsg", "该账号已存在.");
             return map;
         }
-        
+
         //邮箱验证
         u = userMapper.selectByEmail(user.getEmail());
-        if(u!=null){
+        if (u != null) {
             //账号存在
-            map.put("emailMsg","该邮箱已被注册.");
+            map.put("emailMsg", "该邮箱已被注册.");
             return map;
         }
         //此时用户没有问题，开始注册
@@ -85,9 +85,9 @@ public class UserService implements CommunityConstant {
         //需要对user中的数据进行补充，补充需要存到数据库里的，但是不是user输入的
 
         //不需要那么长的salt，5位数就行
-        user.setSalt(CommunityUtil.generateUUid().substring(0,5));
+        user.setSalt(CommunityUtil.generateUUid().substring(0, 5));
         //有了salt对密码进行设置
-        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
         //设置其他字段
         user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
         user.setCreateTime(new Date());
@@ -99,32 +99,32 @@ public class UserService implements CommunityConstant {
 
         //注册，我们要给用户发激活邮件，一定是HTML格式，才可以携带激活链接
         //模板是：activation.html
-        Context context  = new Context();
+        Context context = new Context();
         //传递值给模板
-        context.setVariable("email",user.getEmail());
+        context.setVariable("email", user.getEmail());
         //传递的URL格式：http ://localhost:15213/community/activation/101/code
-        String url = domain + contextPath + "/activation/" + user.getId()+"/"+user.getActivationCode();
-        context.setVariable("url",url);
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
         //第一个参数传入处理的模板是什么
         String content = templateEngine.process("/mail/activation", context);
-        mailClient.sendMail(user.getEmail(),"激活邮箱",content);
+        mailClient.sendMail(user.getEmail(), "激活邮箱", content);
         return map;
         //此时Service层已经完成，去开发控制器Controller
     }
 
     //activate by email method
     //http://localhost:15213/community/activation/101/code
-    public int activate(int userId,String code){
+    public int activate(int userId, String code) {
         User user = userMapper.selectById(userId);
         //如果已经是正式用户，那就是重复激活
-        if(user.getStatus()==1){
+        if (user.getStatus() == 1) {
             return ACTIVATION_REPEAT;
-        }else if(user.getActivationCode().equals(code)){
+        } else if (user.getActivationCode().equals(code)) {
             //激活没问题,status=1是正式用户
             //不能只user.setStatus,这是要更新数据库的操作，需要使用DA层，也就是使用UserMapper
-            userMapper.updateStatus(userId,1);
+            userMapper.updateStatus(userId, 1);
             return ACTIVATION_SUCCESS;
-        }else {
+        } else {
             //activateCode is incorrect
             return ACTIVATION_FAILED;
         }
@@ -139,43 +139,42 @@ public class UserService implements CommunityConstant {
     //如果存在，需要返回一个唯一的ticket。
 
 
-
     /**
-     * @param username just username
-     * @param password these params were from the view layer
+     * @param username       just username
+     * @param password       these params were from the view layer
      * @param expiredSeconds hope to pass how many seconds the credentials will expire,here change it's type from int to long
      *                       because uses int it will over flow
      * @return return map type can record many kinds of messages
      */
-    public Map<String,Object> login(String username,String password,long expiredSeconds){
+    public Map<String, Object> login(String username, String password, long expiredSeconds) {
         //don't know what to do ,let's init the map
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         // 空值处理
-        if(StringUtils.isBlank(username)){
-            map.put("usernameMsg","账号不能为空");
+        if (StringUtils.isBlank(username)) {
+            map.put("usernameMsg", "账号不能为空");
             return map;
         }
-        if(StringUtils.isBlank(password)){
-            map.put("passwordMsg","密码不能为空");
+        if (StringUtils.isBlank(password)) {
+            map.put("passwordMsg", "密码不能为空");
             return map;
         }
         // 验证账号
         User user = userMapper.selectByName(username);
-        if(user ==null){
-            map.put("usernameMsg","该用户不存在，请重新注册");
+        if (user == null) {
+            map.put("usernameMsg", "该用户不存在，请重新注册");
             return map;
         }
         //验证密码
         //犯错记录：数据库中存的密码是加密后的，不能直接拿明文比较,而且有salt
-        if(!CommunityUtil.md5(password+user.getSalt()).equals(user.getPassword())){
-            map.put("passwordMsg","密码错误，请重新输入");
+        if (!CommunityUtil.md5(password + user.getSalt()).equals(user.getPassword())) {
+            map.put("passwordMsg", "密码错误，请重新输入");
             return map;
         }
         //第一次忘记了：验证账号状态
         //status 0-未激活，1-已激活
-        if(user.getStatus()==0){
+        if (user.getStatus() == 0) {
             //尚未激活的账号不可以登录
-            map.put("usernameMsg","该账号未激活，请激活后再登录");
+            map.put("usernameMsg", "该账号未激活，请激活后再登录");
             return map;
         }
         //验证也没有问题，可以登录了
@@ -185,13 +184,17 @@ public class UserService implements CommunityConstant {
         loginTicket.setTicket(CommunityUtil.generateUUid());
         loginTicket.setUserId(user.getId());
         loginTicket.setStatus(0);//这里的status不是user表的，是login_ticket的，0-有效，1-无效
-        loginTicket.setExpired(new Date(System.currentTimeMillis()+expiredSeconds*1000));//设置过期时间
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000));//设置过期时间
         loginTicketMapper.insertLoginTicket(loginTicket);//生成凭证并插入表中
         //凭证要发给客户端，所以最后返回的结果里，要把凭证放进map里
-        map.put("ticket",loginTicket.getTicket());
+        map.put("ticket", loginTicket.getTicket());
         return map;
         //接下来就是表现层的故事了，接收到三个值，传回来，交给UserService处理
-
+    }
+    //使登陆凭证失效
+    public void logout(String ticket) {
+        //是不是这个1代表的失效也放到常量接口里最好
+        loginTicketMapper.updateStatus(ticket,1);
     }
 
 
